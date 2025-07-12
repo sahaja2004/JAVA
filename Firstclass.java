93c4c92b06d67efc6ca7f2bbb68a33920e7f6d22
sahaja2004 <sahajashivaram@gmail.com>
2025-07-12 22:24:47 +0530

    output

--- Firstclass.java
+++ Firstclass.java
@@ -1,5 +1,10 @@
 class Firstclass{
     public static void main(String args[]){
+        System.out.print("Hello World :)");//no next line
+        System.out.println("Hello World :)");//nextline
+        System.out.println("Hello World :)");//next line
         System.out.println("Hello World :)");
+        System.out.print("Hello World :)\n");//Also next line
+        System.out.println("Hello World :)\nHello\nSahaja");
     }
 }
