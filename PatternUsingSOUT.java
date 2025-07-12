7ed487103e5dc5e883c4fa6cc82c82ce6d10500e
sahaja2004 <sahajashivaram@gmail.com>
2025-07-12 22:20:52 +0530

    Patterns Using Print

---
+++ PatternUsingSOUT.java
@@ -0,0 +1,18 @@
+public class PatternUsingSOUT {
+    public static void main(String[] args) {
+        //Method1
+        System.out.println("Using Method-1");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        //Method2
+        System.out.println("Using Method-2");
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+
+
+    }
+    
+}
+ 
