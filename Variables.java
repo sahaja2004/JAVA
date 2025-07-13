40d79e66148ad842943dc075ba90154339dd1c85
sahaja2004 <sahajashivaram@gmail.com>
2025-07-13 22:54:07 +0530

    variables practice

---
+++ Variables.java
@@ -0,0 +1,23 @@
+
+public class Variables{
+    public static void main(String args[]){
+        String name="Sahaja";
+        int age=21;
+        double height=5.9;
+        int a=10;
+        int b= 5;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+        float cal=(a*b)/(a-b);
+        System.out.println(cal);
+        //Input
+        
+
+
+
+    }
+}
